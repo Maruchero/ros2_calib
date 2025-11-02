5f4206e5d5100952f9b1fb9d21e53ad9a61db42c
Marcello Garonzi <marce.gnz303@gmail.com>
2025-11-02 20:05:30 +0100

    Delete unused code from calib_manager_handler.py

--- ros2_calib/dataload/calib_manager_handler.py
+++ ros2_calib/dataload/calib_manager_handler.py
@@ -44,10 +44,6 @@ class CalibManagerHandler:
             raise FileNotFoundError(f"Calib manager file not found: {filepath}")
         self.filepath = filepath
         self.cameras = {}
-        self.transformations = {}
-        # TF graph is structured like this because a child can have only one parent
-        self.tf_graph_fw = defaultdict(list)
-        self.tf_graph_bw = {}
         self.tf_tree = {}
         self.load_calib_manager()
         print("[DEBUG] Correctly loaded calib_manager.yaml file.")
@@ -103,53 +99,6 @@ class CalibManagerHandler:
         for tf_data in tf_entries:
             child = tf_data["targetLink"]
             parent = tf_data["baseLink"]
-            transformation = Transformation(
-                tf_data["tf"][:3],
-                tf_data["tf"][3:],
-            )
-            self.transformations[child] = transformation
-            self.tf_graph_fw[parent].append(child)
-            self.tf_graph_bw[child] = parent
-
-            # Build tf_tree for easier traversal
             self.tf_tree.setdefault(parent, {})[child] = {
                 "transform": self._pose_to_transform(tf_data["tf"])
             }
-
-    def find_transform_path(
-        self, start_frame: str, end_frame: str
-    ) -> Tuple[list, list]:
-        """
-        Find the transformation path from start_frame to end_frame.
-        Returns two lists of transformations: one for backward and one for forward transformations.
-        """
-        # Backward: at first I need to find the path to the common ancestor
-        current_frame = start_frame
-        backward_tfs = [current_frame]
-        while current_frame in self.tf_graph_bw:  # While there is a parent
-            parent_frame = self.tf_graph_bw[current_frame]
-            backward_tfs.append(parent_frame)
-            if parent_frame == end_frame:
-                return backward_tfs, []
-            current_frame = parent_frame
-
-        # Forward: now I need to find the path to the end_frame
-        forward_tfs = []
-        current_frame = backward_tfs[-1] if backward_tfs else start_frame
-        found = True
-        while current_frame != end_frame and found:
-            found = False
-            for child in self.tf_graph_fw[current_frame]:
-                forward_tfs.append(self.transformations[child])
-                current_frame = child
-                found = True
-                break
-
-        if not found:
-            print(
-                f"[ERROR] No transformation path found from {start_frame} to {end_frame}."
-            )
-            return [], []
-        forward_tfs.append(current_frame)
-
-        return backward_tfs, forward_tfs
